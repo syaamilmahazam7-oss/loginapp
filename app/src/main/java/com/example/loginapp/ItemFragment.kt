9140d762823dffa28504a9a4d833e7bc8daff0e0
syaamilmahazam7-oss <syaamilmahazam7@gmail.com>
2026-09-02 12:55:01 +0800

    Create ItemFragment.kt

---
+++ app/src/main/java/com/example/loginapp/ItemFragment.kt
@@ -0,0 +1,32 @@
+package com.example.loginapp
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.loginapp.databinding.FragmentItemBinding
+
+class ItemFragment : Fragment() {
+
+    private var _binding: FragmentItemBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentItemBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.rvItemApps.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
