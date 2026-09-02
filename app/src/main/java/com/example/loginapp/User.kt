8da75afcaffaf5935aebfaa87cc649a90142f438
syaamilmahazam7-oss <syaamilmahazam7@gmail.com>
2026-09-02 12:27:20 +0800

    Update User.kt

--- app/src/main/java/com/example/loginapp/User.kt
+++ app/src/main/java/com/example/loginapp/User.kt
@@ -3,5 +3,6 @@ package com.example.loginapp
 data class User(
     val uid: String = "",
     val name: String = "",
-    val email: String = ""
+    val email: String = "",
+    val role: String = "user"
 )
